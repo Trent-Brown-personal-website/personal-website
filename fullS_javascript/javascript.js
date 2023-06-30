aa112f343f6ad1c322319820d3413e9712072dea
CountSamar <trentjohnbrown@gmail.com>
2023-06-30 03:23:14 -0500

    personal website information added

---
+++ fullS_javascript/javascript.js
@@ -0,0 +1,71 @@
+
+
+
+//This code below is for the section 'Is Truthy"
+
+let input = 0
+
+if ( input ) {
+    console.log(true)
+} else if (input==="") {
+    console.log('The empty string is falsy (the only flasy string)')
+} else if (input === 0 ) {
+    console.log('The number 0 is falsy (the only falsy number)')
+}  else if (input === undefined) {
+    console.log('undefined is falsy')
+} else if ( input === null ) {
+    console.log('The null value is falsy')
+} else if ( input === false ) {
+    console.log('The boolean value false is falsy')
+}
+
+//This code below is for the section 'Number line'
+
+let num1 = 0
+let num2 = 51
+const sum = num1 + num2
+
+if ( sum < -1000 ) {
+    console.log( sum + " is less than -1000")
+} else if ( sum < 0 ) {
+    console.log( sum + ' is a negative number') 
+} else if ( sum === 0 ) {
+    console.log(sum + ' is equal to 0')
+} else if ( sum > 0 && sum < 100 ) {
+    console.log(sum + ' is greater than 0')
+}
+
+//This code below is for the section 'Greater than 5'
+
+let numb1 = -5
+let numb2 = 5
+
+
+if ( numb1 > numb2) {
+    console.log(true)
+} else if ( numb1 < numb2) {
+    console.log(false)
+} else if ( numb1 == numb2 ) {
+    console.log(false)
+} 
+
+let string = [cat, dog, horse, five, dawg, eight, ate, eleven, four, cake, pie]
+let number = [6, 5, 0, 11]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
